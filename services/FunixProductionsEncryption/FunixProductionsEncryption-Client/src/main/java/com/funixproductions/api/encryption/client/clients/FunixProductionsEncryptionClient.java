6b779db76cf86f69f527d54f699549215149d7a3
apronnier <apronnier@groupeastek.fr>
2023-06-26 11:20:45 +0200

    WIP

---
+++ services/FunixProductionsEncryption/FunixProductionsEncryption-Client/src/main/java/com/funixproductions/api/encryption/client/clients/FunixProductionsEncryptionClient.java
@@ -0,0 +1,4 @@
+package com.funixproductions.api.encryption.client.clients;
+
+public interface FunixProductionsEncryptionClient {
+}
---
+++ services/FunixProductionsEncryption/FunixProductionsEncryption-Service/src/main/java/com/funixproductions/api/encryption/service/components/FunixProductionsEncryption.java
@@ -0,0 +1,8 @@
+package com.funixproductions.api.encryption.service.components;
+
+import com.funixproductions.core.tools.encryption.Encryption;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FunixProductionsEncryption extends Encryption {
+}
