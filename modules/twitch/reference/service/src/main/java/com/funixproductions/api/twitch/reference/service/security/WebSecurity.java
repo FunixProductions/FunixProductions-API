00e303e242164e39003ce4119015713f67abcdc6
FunixG <antoine.pronnier@gmail.com>
2023-07-18 15:17:07 +0200

    disable auto conf

---
+++ modules/twitch/reference/service/src/main/java/com/funixproductions/api/twitch/reference/service/security/WebSecurity.java
@@ -0,0 +1,19 @@
+package com.funixproductions.api.twitch.reference.service.security;
+
+import com.funixproductions.api.user.client.security.ApiWebSecurity;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends ApiWebSecurity {
+    @NotNull
+    @Override
+    public Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> getUrlsMatchers() {
+        return ex -> ex.anyRequest().permitAll();
+    }
+}
