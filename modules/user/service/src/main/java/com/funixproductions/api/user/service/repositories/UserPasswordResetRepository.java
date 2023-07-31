1df3bc9af0f7cd89a2b52bc2b02cf75d39e2a0ff
FunixG <antoine.pronnier@gmail.com>
2023-07-31 13:34:34 +0200

    WIP : Done service, need testing

--- modules/user/service/src/main/java/com/funixproductions/api/user/service/repositories/UserPasswordResetRepository.java
+++ modules/user/service/src/main/java/com/funixproductions/api/user/service/repositories/UserPasswordResetRepository.java
@@ -4,6 +4,11 @@ import com.funixproductions.api.user.service.entities.UserPasswordReset;
 import com.funixproductions.core.crud.repositories.ApiRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserPasswordResetRepository extends ApiRepository<UserPasswordReset> {
+
+    Optional<UserPasswordReset> findByResetToken(String resetToken);
+
 }
--- modules/user/service/src/main/java/com/funixproductions/api/user/service/services/UserResetService.java
+++ modules/user/service/src/main/java/com/funixproductions/api/user/service/services/UserResetService.java
@@ -3,27 +3,43 @@ package com.funixproductions.api.user.service.services;
 import com.funixproductions.api.core.enums.FrontOrigins;
 import com.funixproductions.api.google.gmail.client.clients.GoogleGmailClient;
 import com.funixproductions.api.google.gmail.client.dto.MailDTO;
+import com.funixproductions.api.google.gmail.client.utils.GmailUtils;
 import com.funixproductions.api.user.client.dtos.requests.UserPasswordResetDTO;
 import com.funixproductions.api.user.client.dtos.requests.UserPasswordResetRequestDTO;
+import com.funixproductions.api.user.service.components.UserPasswordUtils;
 import com.funixproductions.api.user.service.entities.User;
 import com.funixproductions.api.user.service.entities.UserPasswordReset;
 import com.funixproductions.api.user.service.repositories.UserPasswordResetRepository;
 import com.funixproductions.api.user.service.repositories.UserRepository;
+import com.funixproductions.core.exceptions.ApiBadRequestException;
 import com.funixproductions.core.exceptions.ApiException;
 import com.funixproductions.core.tools.string.PasswordGenerator;
+import com.google.common.base.Strings;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserResetService {
 
+    private static final String FILEPATH_RESET_MAIL = "user/reset-mail.html";
     private final UserRepository userRepository;
+    private final UserPasswordUtils userPasswordUtils;
     private final UserPasswordResetRepository userPasswordResetRepository;
     private final GoogleGmailClient googleGmailClient;
 
@@ -37,8 +53,32 @@ public class UserResetService {
         }
     }
 
+    @Transactional
     public void resetPassword(final UserPasswordResetDTO request) {
+        try {
+            userPasswordUtils.checkPassword(request.getNewPassword());
+            if (!request.getNewPassword().equals(request.getNewPasswordConfirmation())) {
+                throw new ApiBadRequestException("Les mots de passe ne correspondent pas.");
+            }
 
+            final String resetToken = new String(Base64.getDecoder().decode(request.getResetToken()));
+            final UserPasswordReset userPasswordReset = this.userPasswordResetRepository.findByResetToken(resetToken)
+                    .orElseThrow(() -> new ApiBadRequestException("Le token de réinitialisation est invalide."));
+
+            final User user = userPasswordReset.getUser();
+
+            user.setPassword(request.getNewPassword());
+
+            this.userRepository.save(user);
+            this.userPasswordResetRepository.delete(userPasswordReset);
+        } catch (ApiException e) {
+            throw e;
+        } catch (Exception e) {
+            final String errorMessage = "Erreur interne lors de la réinitialisation du mot de passe.";
+
+            log.error(errorMessage, e);
+            throw new ApiException(errorMessage, e);
+        }
     }
 
     private MailDTO generateResetMail(final User user, final FrontOrigins origin) {
@@ -54,7 +94,49 @@ public class UserResetService {
     private String generateResetMailBody(final User user,
                                          final FrontOrigins origin,
                                          final String resetToken) {
-        //TODO read reset-mail.html and replace placeholders
+        final String urlRedirect = getUrlRedirect(origin, resetToken);
+
+        try (final InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(FILEPATH_RESET_MAIL)) {
+            if (inputStream != null) {
+                try (final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                    final StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+
+                    return GmailUtils.minifyHtml(stringBuilder.toString()
+                            .replace("{{USERNAME}}", user.getUsername())
+                            .replace("{{URL_PASSWORD_RESET}}", urlRedirect)
+                            .replace("{{ORIGIN_WEBSITE}}", origin.getHumanReadableOrigin()));
+                }
+            } else {
+                final String errorMessage = "Erreur interne lors de la génération du corps du mail de réinitialisation de mot de passe. InputStream null read reset-mail.html.";
+
+                log.error(errorMessage);
+                throw new ApiException(errorMessage);
+            }
+        } catch (Exception e) {
+            final String errorMessage = "Erreur interne lors de la génération du corps du mail de réinitialisation de mot de passe.";
+
+            log.error(errorMessage, e);
+            throw new ApiException(errorMessage, e);
+        }
+    }
+
+    private String getUrlRedirect(final FrontOrigins origin, final String resetToken) {
+        final String env = System.getenv("SPRING_PROFILES_ACTIVE");
+        final boolean isDev = !Strings.isNullOrEmpty(env) && env.contains("dev");
+        final String urlRedirect;
+
+        if (isDev) {
+            urlRedirect = origin.getDomainDev();
+        } else {
+            urlRedirect = origin.getDomainProd();
+        }
+
+        return urlRedirect + "/reset-password?token=" + Base64.getEncoder().encodeToString(resetToken.getBytes());
     }
 
     private UserPasswordReset generateNewResetToken(final User user, final FrontOrigins origin) {
@@ -69,6 +151,27 @@ public class UserResetService {
             return this.userPasswordResetRepository.save(passwordReset);
         } catch (Exception e) {
             final String errorMessage = "Erreur interne lors de la génération du token de réinitialisation de mot de passe.";
+
+            log.error(errorMessage, e);
+            throw new ApiException(errorMessage, e);
+        }
+    }
+
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
+    public void checkExpiredPasswordResetTokens() {
+        try {
+            final Instant instant = Instant.now();
+            final Iterable<UserPasswordReset> userPasswordResets = this.userPasswordResetRepository.findAll();
+            final Set<UserPasswordReset> expiredTokens = new HashSet<>();
+
+            for (final UserPasswordReset userPasswordReset : userPasswordResets) {
+                if (userPasswordReset.getCreatedAt().toInstant().plus(10, ChronoUnit.MINUTES).isBefore(instant)) {
+                    expiredTokens.add(userPasswordReset);
+                }
+            }
+            this.userPasswordResetRepository.deleteAll(expiredTokens);
+        } catch (Exception e) {
+            final String errorMessage = "Erreur interne lors de la vérification des tokens de réinitialisation de mot de passe expirés.";
 
             log.error(errorMessage, e);
             throw new ApiException(errorMessage, e);
