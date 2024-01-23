c973bfabb1fe3e1159295e6d81b2337e943f9994
FunixG <antoine.pronnier@gmail.com>
2024-01-23 21:57:59 +0100

    done ressource for user and admin and kubernetes

--- modules/payment/billing/client/src/main/java/com/funixproductions/api/payment/billing/client/clients/BillingClient.java
+++ modules/payment/billing/client/src/main/java/com/funixproductions/api/payment/billing/client/clients/BillingClient.java
@@ -1,7 +1,24 @@
 package com.funixproductions.api.payment.billing.client.clients;
 
 import com.funixproductions.api.payment.billing.client.dtos.BillingDTO;
-import com.funixproductions.core.crud.clients.CrudClient;
+import com.funixproductions.core.crud.dtos.PageDTO;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-public interface BillingClient extends CrudClient<BillingDTO> {
+public interface BillingClient {
+
+    @GetMapping
+    PageDTO<BillingDTO> getAll(@RequestParam(value = "page", defaultValue = "0") String page,
+                               @RequestParam(value = "elemsPerPage", defaultValue = "300") String elemsPerPage,
+                               @RequestParam(value = "search", defaultValue = "") String search,
+                               @RequestParam(value = "sort", defaultValue = "") String sort);
+
+    @GetMapping("{id}")
+    BillingDTO findById(@PathVariable("id") String id);
+
+    @GetMapping("{id}/invoice")
+    Resource downloadInvoice(@PathVariable String id);
+
 }
